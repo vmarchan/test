e067c2996f15383a42b4bae41bc78e3148e6216f
Veronica Marchan <veronica.marchan.sanchez@everis.com>
2016-02-28 17:34:54 +0100

    Initial Commit

---
+++ gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by vmarchan on 28/2/16.
+//  Copyright © 2016 vmarchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by vmarchan on 28/2/16.
+//  Copyright © 2016 vmarchan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
